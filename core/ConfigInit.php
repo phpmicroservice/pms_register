6584dd58fa540ef106ea63f7a68cfb9fe6fc07a2
Dongasai <1514582970@qq.com>
2018-03-26 17:14:36 +0800

    01

---
+++ core/ConfigInit.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace core;
+
+/**
+ * 配置
+ * Class Config
+ * @package core
+ */
+class ConfigInit
+{
+
+}
---
+++ core/Log.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Dongasai
+ * Date: 2018/3/26
+ * Time: 16:19
+ */
+
+namespace core;
+
+
+class log
+{
+
+}
