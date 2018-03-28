5e0364a1b9d951b0b664dd4377a637a0d4506aed
Dongasai <1514582970@qq.com>
2018-03-28 18:57:59 +0800

    2018年3月28日18:57:55

--- core/ConfigInit.php
+++ core/ConfigInit.php
@@ -24,17 +24,17 @@ class ConfigInit extends Base
         $this->config_port = get_env('CONFIG_PORT', '9502');
         $this->server = $server;
         $this->init();
-        $ConfigInit=$this;
+        $ConfigInit = $this;
 
-        swoole_timer_tick(5000, function($id)use($ConfigInit,$server) {
+        swoole_timer_tick(5000, function ($id) use ($ConfigInit, $server) {
             $ConfigInit->demo();
             echo "config server judge \n";
-            $cache=\Phalcon\Di::getDefault()->get('cache');
+            $cache = \Phalcon\Di::getDefault()->get('cache');
             # 确认初始化已经完成,更新配置信息
             if (!$cache->get('cache_init')) {
                 # 没有初始化完毕
                 $ConfigInit->init();
-            }else{
+            } else {
                 # 完成初始化了
                 $ConfigInit->update();
             }
@@ -46,7 +46,7 @@ class ConfigInit extends Base
     {
         echo "ConfigInit -> init \n";
         $this->get_swoole_client();
-        echo $this->config_ip .':'. $this->config_port . " \n";
+        echo $this->config_ip . ':' . $this->config_port . " \n";
 
         if (self::$config_client->isConnected()) {
             self::$config_client->send('{"r":"config_acquire","d":"register"}');
@@ -64,6 +64,20 @@ class ConfigInit extends Base
         } else {
             self::$config_client = new \swoole_client(SWOOLE_SOCK_TCP, SWOOLE_SOCK_ASYNC);
         }
+        self::$config_client->set([
+            'open_length_check' => true,
+            'package_length_func' => function ($data) {
+                var_dump($data);
+                if (strlen($data) < 8) {
+                    return 0;
+                }
+                $length = intval(trim(substr($data, 0, 8)));
+                if ($length <= 0) {
+                    return -1;
+                }
+                return $length + 8;
+            },
+        ]);
 
         self::$config_client->on("connect", [$this, 'connect']);
         self::$config_client->on("receive", [$this, 'receive']);
@@ -82,9 +96,8 @@ class ConfigInit extends Base
     }
 
 
-
     /**
-     * 收到返回值
+     * 收到值
      * @param \swoole_client $cli
      * @param $data
      */
@@ -120,12 +133,15 @@ class ConfigInit extends Base
         self::$config_client->send('{"r":"config_renewal","d":"register"}');
     }
 
-    public function ping(){
+    public function ping()
+    {
         echo "ConfigInit ping ... \n";
         self::$config_client->send('{"r":"ping_ping","d":""}');
     }
-    public function demo(){
+
+    public function demo()
+    {
         echo "ConfigInit demo ... \n";
-        self::$config_client->send('4545}');
+        self::$config_client->send('4545');
     }
 }
