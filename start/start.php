6a83c357282f7f086e8c4f0958e008e2acdeb9d4
Dongasai <1514582970@qq.com>
2018-04-24 16:33:49 +0800

    2018年4月20日09:56:59

--- start/start.php
+++ start/start.php
@@ -25,8 +25,7 @@ $loader->register();
 
 $server = new \pms\Server('0.0.0.0', 9502, SWOOLE_BASE, SWOOLE_SOCK_TCP, [
     'daemonize' => false,
-    'worker_num' => 2,
-    'task_worker_num' => 2,
+    'task_worker_num' => 4,
     'reload_async' => false,
     'open_eof_split' => true, //打开EOF检测
     'package_eof' => PACKAGE_EOF, //设置EOF
